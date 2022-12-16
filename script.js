c5ed4972dec1efd2a7b311e9dae9e4d38fe4b717
kacperlNYC <113933880+kacperlNYC@users.noreply.github.com>
2022-12-16 14:05:07 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function init()
 {
+    messageOut = document.getElementById("message");
     tablePlayerOut = document.getElementById("playerTable");
     tableComputerOut = document.getElementById("computerTable");
     deckPlayer = [5,5,5]; // [r,p,s]
     deckComputer = [5,5,5];
+    message = "";
     
     resetTable();
 }
@@ -26,4 +28,64 @@ function resetTable()
         rowComputer.insertCell().innerHTML = "<img src='img/scissor.png'>"
 
     }
+}
+
+function play(choice)
+{
+    computerChoice = Math.floor(Math.random() * 3)
+    // 0 Rock
+    // 1 Paper
+    // 2 Scissors
+    if (choice === "Rock")
+    {
+        if (computerChoice === 0)
+        {
+            message = "Rock & Rock: Tie"
+        }
+        else if (computerChoice === 1)
+        {
+            message = "Rock loses to Paper: Computer Wins"
+        }
+        else 
+        {
+            message = "Rock wins to Scissors: Player Wins"
+        }
+    }
+    else if (choice === "Paper")
+    {
+        if (computerChoice === 0)
+        {
+            message = "Paper wins to Rock: Player Wins"
+        }
+        else if (computerChoice === 1)
+        {
+            message = "Paper & Paper: Tie"
+        }
+        else 
+        {
+            message = "Paper loses to Scissors: Computer Wins"
+        }
+    }
+    else
+    {
+        if (computerChoice === 0)
+        {
+            message = "Scissors loses to Rock: Computer Wins"
+        }
+        else if (computerChoice === 1)
+        {
+            message = "Scissors wins to Paper: Player Wins"
+        }
+        else 
+        {
+            message = "Scissors & Scissors: Tie"
+        }
+    }
+    
+    display();
+}
+
+function display()
+{
+    messageOut.innerHTML = message;
 }
